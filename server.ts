7e6b609847271f9c07c59a6d1749904dde09dc1a
evandroneder <24230264+evandroneder@users.noreply.github.com>
2020-03-03 16:14:30 -0300

    dist

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import * as fs from "fs";
-import { MongoClient, Db, Collection } from "mongodb";
+import * as db from "./mongo";
 
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -13,9 +13,9 @@ app.use(bodyParser.json());
 export async function StartServer(config: {
   port?: number;
   controllersPath: string;
-  mongoDB: ICfgMongo;
+  mongoDB: db.ICfgMongo;
 }) {
-  await startClient(config.mongoDB);
+  await db.startClient(config.mongoDB);
 
   const port = config.port || defaultPort;
 
@@ -35,54 +35,6 @@ async function processRoutePath(route_path) {
       app.use("/" + name, require(filepath));
     }
   });
-}
-
-export interface ICfgMongo {
-  url: string;
-  useNewUrlParser: boolean;
-  useUnifiedTopology: boolean;
-}
-
-var CONFIG: ICfgMongo;
-
-var CLIENT;
-
-export async function startClient(cfg: ICfgMongo) {
-  CONFIG = Object.assign({}, cfg);
-  await getClient();
-}
-
-async function getClient(): Promise<MongoClient> {
-  return (CLIENT = await MongoClient.connect(CONFIG.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }).catch(err => {
-    console.log(err);
-    throw err;
-  }));
-}
-
-export async function getColletion(config: { db: string; collection: string }) {
-  const client = CLIENT;
-  var db: Db;
-  var collection: Collection;
-  try {
-    db = await client.db(config.db);
-    collection = await db.collection(config.collection);
-  } catch (e) {
-    console.log(e);
-    client.close();
-    throw e;
-  }
-
-  return { client, db, collection };
-}
-
-export function handleServerError(error: any) {
-  return {
-    status: 500,
-    error: error
-  };
 }
 
 /*
